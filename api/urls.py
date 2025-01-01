84b98334b2832d11a18df7fc145b843f9da5c776
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-01-01 22:06:05 +0500

    Generics Views

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogposts/',views.BlogPostListCreate.as_view())
+    path('blogposts/',views.BlogPostListCreate.as_view()),
+    path('blogposts/<int:pk>/',views.BlogPostRetrieveUpdateDestroy.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,17 @@ from rest_framework import generics
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
-#  Views...
+#  View for Getting and Creating BlogPost
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+    
+    
+    
+
+#  View for Updating and deleting and getting single post
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = 'pk' 
